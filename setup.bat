d1be56160e84e1086eed810301b02642e1ffa750
Florian-EL <florian.eymerleprovost@gmail.com>
2025-12-02 23:27:00 +0100

    setup linux et windows

--- setup.bat
+++ setup.bat
@@ -1,5 +1,6 @@
 @echo off
-cd /d C:\Users\Florian\Documents\TransportApp
+cd /d "C:\Users\Florian\Documents\TransportApp"
+xcopy /E /Y /I /Q ".\build\exe.win-amd64-3.10\data\*" ".\data\"
 call venv\Scripts\activate.bat
 python build_exe.py build
 call venv\Scripts\deactivate.bat
